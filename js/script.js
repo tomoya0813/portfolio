1c61bfa105f10e16828df95531448709de8b174f
ともや <tangkoup@gmail.com>
2026-07-26 04:06:13 +0900

    brタグをレスポンシブ対応する処理を追加

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 'use strict'
-
+//メニューボタン
 const btn = document.getElementById('btn');
 const nav = document.getElementById('nav')
 const moveNav = () => {
@@ -8,3 +8,37 @@ const moveNav = () => {
 }
 
 btn.addEventListener('click', moveNav)
+
+//br削除
+
+const mediaQuery = window.matchMedia('(max-height : 630px)');
+const NEWbr = '<br>';
+const indent = document.querySelectorAll('.indent');
+
+
+const deleteBr = (e) => {
+    e.forEach((e) => {
+        e.remove();
+    })
+}
+
+const addBr = () => {
+
+    indent.forEach((e) => {
+        e.insertAdjacentHTML("beforebegin", NEWbr)
+    });
+}
+
+const checkBr = () => {
+    const br = document.querySelectorAll('.hero br');
+    if (mediaQuery.matches) {
+        if (br.length === 0) return;
+        deleteBr(br);
+    } else {
+        if (br.length !== 0) return;
+        addBr();
+    }
+}
+
+window.addEventListener('DOMContentLoaded', checkBr);
+mediaQuery.addEventListener('change', checkBr);
