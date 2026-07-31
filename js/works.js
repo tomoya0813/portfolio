4aff57292a910fbe20bd2b8e6b19b6bdc1bd25fc
ともや <tangkoup@gmail.com>
2026-08-01 04:25:55 +0900

    feat; works完成

--- js/works.js
+++ js/works.js
@@ -7,14 +7,19 @@ fetch("../data/data.json")
     const works = data.works;
     works.forEach(e => {
 
+      const techs = (e.tech || [])
+        .map(techName => `<span class="tech-tag">${techName}</span>`)
+        .join('');
+
       const imgSrc = `../${e.imgSrc}`
 
-      const addHTML = ` <div class="works-item">
+      const addHTML = ` <div class="works-item">                          
                             <p class="works-title">${e.title}</p>
-                            <img src="${imgSrc}" alt="${e.imgALT}">
+                              ${techs}
+                            <img src="${imgSrc}" alt="${e.imgAlt}">
                             <p class="works-text">${e.text}</p>
                           <div class="btn-wrapper">
-                              <a href="${e.link}" class="demo btn">Viwe Demo</a>
+                              <a href="${e.link}" class="demo btn">View Demo</a>
                               <a href="${e.githubLink}" class="github btn">Github Code</a>
                           </div>
                         </div>`
