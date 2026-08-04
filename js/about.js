aea01f1e0ad3dfaf417809d949972f14ffbfaaf7
ともや <tangkoup@gmail.com>
2026-08-05 04:43:01 +0900

    feat: aboutページ完成

--- js/about.js
+++ js/about.js
@@ -28,10 +28,10 @@ fetch("../data/data.json")
         const journey = data.about.journey;
         const journeyHTML = journey
             .map(journeyItem =>
-                `<p>
-                     <span class="date">${journeyItem.date}</span><br>
-                     <span class="text">${journeyItem.text}</span>
-                 </p>`)
+                `<div class="journey-item">
+                        <p class="date">${journeyItem.date}</p>
+                        <p class="journey-text">${journeyItem.text}</p>
+                </div>`)
             .join('');
         journeyContainer.insertAdjacentHTML('beforeend', journeyHTML);
     });
