3b979ed1f5cca537a2480c303c1e4261df3665d5
ともや <tangkoup@gmail.com>
2026-08-08 09:43:09 +0900

    feat: 不要な関数削除、時計設定追加、画像読み込み処理追加

--- js/top.js
+++ js/top.js
@@ -1,82 +1,60 @@
 'use strict'
+// 時計設定
+document.querySelector('.bg').append(createClock({
+    height: 500,
+    width: 500,
+    opacity: 0.3,
+    borderRadius: 50,
+    borderColor: '#4784BF',
+    backgroundColor: '#4783bf62',
+    borderWidth: 2,
+
+    minuteHand: {
+        width: 230,
+        height: 5,
+        opacity: 1,
+        color: "#4784BF"
+    },
+
+    hourHand: {
+        width: 170,
+        height: 5,
+        opacity: 1,
+        color: "#4784BF"
+    },
+
+    secondHand: {
+        width: 230,
+        height: 2,
+        opacity: 1,
+        color: "#4784BF"
+    }
+}));
+
 // works取り込み
+const worksContainer = document.querySelector('.splide__list');
 fetch("data/data.json")
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) throw new Error('サーバーエラー')
+        return response.json()
+    })
     .then(data => {
-        sessionStorage.setItem("works", JSON.stringify(data.works))
+        sessionStorage.setItem("works", JSON.stringify(data.works));
+
+        const worksHTML = data.works
+            .map(work =>
+                `<li class="splide__slide"><img src="${work.imgSrc}" alt ="${work.imgAlt}"></li>`
+            )
+            .join('');
+        worksContainer.insertAdjacentHTML('beforeend', worksHTML);
+        new Splide('.splide', {
+            type: 'loop',
+            rewind: true,
+            autoplay: true,
+            interval: 5000
+        }).mount();
     })
-//ios確認
-const ua = window.navigator.userAgent;
-
-const isIos =
-    ua.includes('iPhone') ||
-    ua.includes('iPad') ||
-    (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
-
-//br削除
-const mqBr = window.matchMedia('(max-height : 630px) and (min-width:400px)');
-const brHTML = '<br>';
-const indent = document.querySelectorAll('.indent');
-
-const deleteBr = () => {
-    document.querySelectorAll('.hero br').forEach((e) => {
-        e.remove();
+    .catch(error => {
+        window.alert('作品データを読み込めませんでした。')
     })
-}
-
-const addBr = () => {
-    indent.forEach((e) => {
-        e.insertAdjacentHTML("beforebegin", brHTML)
-    });
-}
-
-const checkBr = () => {
-    const hasBr = document.querySelectorAll('.hero br').length !== 0;
-
-    if (mqBr.matches && hasBr) {
-        deleteBr();
-    } else if (!mqBr.matches && !hasBr) {
-        addBr();
-    }
-}
-
-// triangle移動
-const hero = document.querySelector('.hero');
-const subTitle = document.querySelector('.sub-title')
-const mqTri = window.matchMedia('(max-width:1024px)');
-const triangle = document.querySelector('.triangle')
-
-const moveTriangle = () => {
-    if (mqTri.matches) {
-        subTitle.appendChild(triangle)
-    } else {
-        hero.appendChild(triangle)
-    }
-}
-
-// ナビのクラス削除
-
-// const navLinks = document.querySelectorAll('.page-nav li a');
-// const mqNav = window.matchMedia('(max-width: 1000px)');
-
-// const toggleNavLinks = () => {
-//     navLinks.forEach((e) => {
-//         e.classList.toggle('inversion', !mqNav.matches);
-//     });
-// }
-
-
-// 実行
-window.addEventListener('DOMContentLoaded', () => {
-    if (isIos) {
-        document.body.classList.add('ios')
-    }
-    checkBr();
-    moveTriangle();
-    // toggleNavLinks();
-});
-
-mqBr.addEventListener('change', checkBr);
-mqTri.addEventListener('change', moveTriangle);
-// mqNav.addEventListener('change', toggleNavLinks);
 
