71ace2d51657aa6c704e31c76456ac86d79e9e94
ともや <tangkoup@gmail.com>
2026-08-12 22:38:39 +0900

    test: render

--- server/server.js
+++ server/server.js
@@ -4,16 +4,6 @@ const Port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send(`<form action="/contact" method="POST">
-    <input type="text" name="name" placeholder="名前">
-    <input type="email" name="email" placeholder="メールアドレス">
-    <textarea name="message" placeholder="お問い合わせ内容"></textarea>
-
-    <button type="submit">送信</button>
-</form>`);
-
-})
 
 app.post('/contact', (req, res) => {
     console.log('post到着')
