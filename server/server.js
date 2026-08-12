ca01a822a4d23e54b7f47238a601034ffe97d9e4
ともや <tangkoup@gmail.com>
2026-08-12 13:02:58 +0900

    test: expressサーバー追加

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const Port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send(`<form action="/contact" method="POST">
+    <input type="text" name="name" placeholder="名前">
+    <input type="email" name="email" placeholder="メールアドレス">
+    <textarea name="message" placeholder="お問い合わせ内容"></textarea>
+
+    <button type="submit">送信</button>
+</form>`);
+
+})
+
+app.post('/contact', (req, res) => {
+    console.log(req.body);
+    res.send('送信されました')
+})
+
+app.listen(Port, '0.0.0.0', () => {
+    console.log('サーバー起動')
+})
+
