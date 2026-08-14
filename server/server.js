03892294cc7eca72e9d34c0186147cd276d83ac9
ともや <tangkoup@gmail.com>
2026-08-14 19:48:35 +0900

    test: sendmail

--- server/server.js
+++ server/server.js
@@ -14,34 +14,46 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-app.post('/contact', async (req, res) => {
-    console.log('post到着')
-    console.log(req.body);
 
+app.post('/contact', async (req, res) => {
     try {
         const info = await transporter.sendMail({
             from: "tomoya.portfolio.contact@gmail.com",
             to: "tomoya.portfolio.contact@gmail.com",
-            text: ` 
-                名前: ${req.body.name}
-                メール: ${req.body.email}
-                内容: ${req.body.message}
-                `,
-            replyTo: req.body.email
-        })
+            subject: "Hello", // subject line
+            text: "Hello world?", // plain text body
+        });
 
         console.log("Message sent: %s", info.messageId);
+        // Preview URL is only available when using an Ethereal test account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
     } catch (err) {
         console.error("Error while sending mail:", err);
     }
-    res.send('this is test message')
 })
 
-app.get('/oauth2callback', (req, res) => {
-    console.log(req.query.code)
-    res.send(req.query.code)
+// app.post('/contact', async (req, res) => {
+//     console.log('post到着')
 
-})
+//     try {
+//         const info = await transporter.sendMail({
+//             from: "tomoya.portfolio.contact@gmail.com",
+//             to: "tomoya.portfolio.contact@gmail.com",
+//             text: ` 
+//                 名前: ${req.body.name}
+//                 メール: ${req.body.email}
+//                 内容: ${req.body.message}
+//                 `,
+//             replyTo: req.body.email
+//         })
+
+//         console.log("Message sent: %s", info.messageId);
+//     } catch (err) {
+//         console.error("Error while sending mail:", err);
+//     }
+//     res.send('this is test message')
+// })
+
 
 app.listen(Port, '0.0.0.0', async () => {
 
