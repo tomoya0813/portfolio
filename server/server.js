6745cbc943e3b097005e47a54e8cd7d1161764ab
ともや <tangkoup@gmail.com>
2026-08-14 17:51:52 +0900

    test: oauth認証とメール

--- server/server.js
+++ server/server.js
@@ -1,17 +1,56 @@
+const nodemailer = require('nodemailer');
 const express = require('express');
 const app = express();
 const Port = process.env.PORT || 3000;
 
+const transporter = nodemailer.createTransport({
+    service: "gmail", // Shortcut for Gmail's SMTP settings - see Well-Known Services
+    auth: {
+        type: "OAuth2",
+        user: "tomoya.portfolio.contact@gmail.com",
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+    },
+});
+
+
+
 app.use(express.urlencoded({ extended: true }));
 
 
-app.post('/contact', (req, res) => {
+app.post('/contact', async (req, res) => {
     console.log('post到着')
     console.log(req.body);
+
+    try {
+        const info = await transporter.sendMail({
+            from: "tomoya.portfolio.contact@gmail.com",
+            to: "tomoya.portfolio.contact@gmail.com",
+            text: ` 
+                名前: ${req.body.name}
+                メール: ${req.body.email}
+                内容: ${req.body.message}
+                `,
+            replyTo: req.body.email
+        })
+
+        console.log("Message sent: %s", info.messageId);
+    } catch (err) {
+        console.error("Error while sending mail:", err);
+    }
     res.send('this is test message')
 })
 
+app.get('/oauth2callback', (req, res) => {
+    console.log(req.query.code)
+    res.send(req.query.code)
+
+})
+
 app.listen(Port, '0.0.0.0', () => {
+    console.log(process.env.GOOGLE_CLIENT_ID);
     console.log('サーバー起動')
 })
+
 
